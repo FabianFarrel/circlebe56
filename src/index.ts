a66f37d199714d626ef932027cc69fc5e20a3458
FabianFarre1 <fbn.farrel.id@gmail.com>
2024-10-09 22:47:04 +0700

    vrcl

--- src/index.ts
+++ src/index.ts
@@ -9,15 +9,16 @@ import { routerV1 } from "./routes/v1";
 dotenv.config();
 
 const app: Express = express();
-
 app.use(express.json());
+
+
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
     origin: 'http://localhost:5173',  // Frontend URL
     credentials: true,  // If you are sending cookies
 }));
 
-app.use('/uploadImage', express.static('uploadImage'));  // Consider using an external service for this in production
+app.use('/uploadImage', express.static('uploadImage'));  
 app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument, {
     explorer: true,
     swaggerOptions: {
@@ -28,5 +29,5 @@ app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument, {
 app.use("/api/v1", routerV1);
 app.use(errorMiddleware);
 
-// Do NOT call `app.listen()` here
+
 export default app;
