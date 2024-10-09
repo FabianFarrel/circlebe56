84d4e95d2f40e51686e4a11a61c94584990adde0
FabianFarre1 <fbn.farrel.id@gmail.com>
2024-10-09 23:26:09 +0700

    fix vercel

--- src/index.ts
+++ src/index.ts
@@ -10,8 +10,6 @@ dotenv.config();
 
 const app: Express = express();
 app.use(express.json());
-
-
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
     origin: 'http://localhost:5173',  
@@ -26,9 +24,12 @@ app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument, {
         displayRequestDuration: true,
     }
 }));
+
 app.use("/api/v1", routerV1);
 app.use(errorMiddleware);
 
-
-
-export default app;
+// Add this part to start the server
+const PORT = process.env.PORT || 5000; // Use the PORT from your .env file or default to 5000
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT} in ${process.env.NODE_ENV} mode`); // Log server status
+});
