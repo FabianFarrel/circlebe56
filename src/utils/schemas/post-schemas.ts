e1b1c71e27926a9a071196d5295363e128150aff
FabianFarre1 <fbn.farrel.id@gmail.com>
2024-10-18 22:37:11 +0700

    post fixing

--- src/utils/schemas/post-schemas.ts
+++ src/utils/schemas/post-schemas.ts
@@ -2,7 +2,7 @@ import Joi from 'joi'
 import { CreatePostDTO } from '../../dto/post-dto'
 
 export const postSchema = Joi.object<CreatePostDTO>({
-    content: Joi.string().min(5),
+    content: Joi.string().optional().min(5),
     image: Joi.string().optional().allow(null, ''),
     authorId: Joi.number().integer()
 })
